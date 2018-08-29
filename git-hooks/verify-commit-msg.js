bb391f5b6ac716560cf9393a9d44378238638b4c
MoJingHui <369188955@qq.com>
2018-08-29 14:01:59 +0800

    feat: 增加git commit 规范处理

--- git-hooks/verify-commit-msg.js
+++ git-hooks/verify-commit-msg.js
@@ -7,10 +7,10 @@ const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
-  console.error(`  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n${
-    chalk.red('  Proper commit message format is required for automated changelog generation. Examples:\n\n')
+  console.error(`  ${chalk.bgRed.white(' 错误 ')} ${chalk.red('提交信息格式不规范')}\n\n${
+    chalk.red('  建议使用符合规范的提交信息。 例如:\n\n')
   }    ${chalk.green('feat(compiler): add \'comments\' option')}\n` +
     `    ${chalk.green('fix(v-model): handle events on blur (close #28)')}\n\n${
-      chalk.red(`  You can also use ${chalk.cyan('npm run commit')} to interactively generate a commit message.\n`)}` );
+      chalk.red(`  您可以使用命令 ${chalk.cyan('yarn commit/commit:custom')} 生成提交信息。\n`)}` );
   process.exit(1);
 }
