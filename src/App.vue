f4ac24e737bb9cde3c9798b4a1f6e61396861c75
MoJingHui <369188955@qq.com>
2018-08-30 17:20:11 +0800

    feat: test camera

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
+        <code
+          v-text="'<el-button>'"/>
         below
       </p>
       <el-button>el-button</el-button>
@@ -21,7 +22,7 @@
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld,
   },
--- src/modules/goods/views/About1.vue
+++ src/modules/goods/views/About1.vue
@@ -1,5 +1,88 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="dialogVisible = true">打开弹窗</el-button>
+    <el-dialog
+      :visible.sync="dialogVisible"
+      title="摄像头"
+      @open="dialogOpen"
+      @close="closeVideo">
+      <video
+        ref="video"
+        autoplay/>
+
+      <span slot="footer">
+        <el-button @click="closeVideo">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="startVideo">
+          开启
+        </el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    dialogVisible: false,
+
+    videoConstraint: {
+      width: 200,
+      height: 100,
+      frameRate: 30,
+    },
+
+    videoEl: null,
+
+    videoTracks: null,
+  }),
+
+  mounted() {
+    // this.videoEl = this.$refs.video;
+  },
+
+  methods: {
+    dialogOpen() {
+      // console.log(this.$refs);
+      this.$nextTick(() => {
+        this.videoEl = this.$refs.video;
+      });
+      // this.videoEl = this.$refs.video;
+    },
+
+    closeVideo() {
+      this.videoEl.srcObject = null;
+    },
+    startVideo() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: this.videoConstraint,
+          audio: false,
+        })
+        .then((stream) => {
+          const videoTracks = stream.getVideoTracks();
+          this.$nextTick(() => {
+            this.videoEl.srcObject = stream;
+          });
+          if (videoTracks.length) {
+            const [track] = videoTracks;
+            this.videoTrack = track;
+          }
+        })
+        .then(() => {
+          // eslint-disable-next-line
+          new Promise(((resolve) => {
+            this.videoEl.onloadedmetadata = resolve;
+          }));
+        })
+        .then(() => {
+          // getCurrentSettings();
+        })
+        .catch();
+    },
+  },
+};
+</script>
+
