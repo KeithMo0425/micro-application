5194a75997eebbf89575c6961a1bf800010b0b44
MoJingHui <369188955@qq.com>
2018-08-31 09:03:38 +0800

    style: modify

--- src/modules/goods/views/About.vue
+++ src/modules/goods/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
-    <el-input v-model="inputNumber" placeholder=""></el-input>
+    <el-input
+      v-model="inputNumber"
+      placeholder=""/>
     <el-button @click="onClick">测试ffi</el-button>
     <h1>This is an about page</h1>
   </div>
